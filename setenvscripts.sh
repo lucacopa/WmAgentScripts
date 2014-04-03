4a2760c2839260a99ee0d908dec192472e219775
lucacopa <lucacopa@gmail.com>
2014-04-03 14:25:15 -0500

    This allows the environment script to source agent env when it is possible

--- setenvscripts.sh
+++ setenvscripts.sh
@@ -1,5 +1,14 @@
 USER=$(whoami)
 kinit $USER@CERN.CH
 source /afs/cern.ch/project/gd/LCG-share/3.2.11-1/etc/profile.d/grid-env.sh
+{
+source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
+} || {
+echo ‘This machine does not have WMAgent installed. Not sourcing Agent environment’
+}
+{
 git pull
+} || {
+echo ‘This machine does not have git installed. Not updating WmAgentScripts repository’
+}
 voms-proxy-init -voms cms
